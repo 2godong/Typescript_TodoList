bb40ee8d79dd2bd6293a6c1c15fefa5b909547a7
2godong <sora050821@gmail.com>
2026-08-16 23:52:48 +0900

    hello.ts 파일명 변경

---
+++ build/TodoItem.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class TodoItem {
+    id;
+    task;
+    complete;
+    constructor(id, task, complete) {
+        this.id = id;
+        this.task = task;
+        this.complete = complete;
+        this.id = id;
+        this.task = task;
+        this.complete = complete;
+    }
+    printDetails() {
+        console.log(`${this.id}\t${this.task}\t${this.complete ? "\t(complete)" : ""}`);
+    }
+}
+exports.default = TodoItem;
+//# sourceMappingURL=TodoItem.js.map

394943d0098d3ca8abf4ad47d3f6538e02b79f5b
2godong <sora050821@gmail.com>
2026-08-16 23:48:45 +0900

    투두리스트 class ts문법으로 작성

--- src/hello.ts
+++ src/hello.ts
@@ -1,4 +1,16 @@
-console.log("Hello Todo List!");
-console.log("Hello Todo List!");
-console.log("Hello Todo List!");
-console.log("Hello Todo List!");
+// console.log("Hello Todo List!");
+// console.log("Hello Todo List!");
+// console.log("Hello Todo List!");
+// console.log("Hello Todo List!");
+import TodoItem from "./TodoItem";
+
+const data = [
+  { id: 1, task: "장보기", complete: true },
+  { id: 2, task: "TS 째려보기", complete: false },
+];
+
+console.log("My Todo List");
+for (let i = 0; i < data.length; i++) {
+  let todoItem = new TodoItem(data[i].id, data[i].task, data[i].complete);
+  todoItem.printDetails();
+}
